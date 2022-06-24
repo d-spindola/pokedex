5827a88266c421633960a97af7e80373a32a6b2a
EhOPikachu <davispindola@proton.me>
2022-06-24 06:07:50 -0300

    add some global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import { Global } from "@emotion/react";
+import globalStyles from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />;
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
