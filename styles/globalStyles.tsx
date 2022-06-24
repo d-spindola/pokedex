fd3a0ffa16e700dbe3c66c48518247d94ec6d9d1
EhOPikachu <davispindola@proton.me>
2022-06-24 06:08:37 -0300

    create global styles using css emotion

---
+++ styles/globalStyles.tsx
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+export default css`
+  * {
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
+`;
