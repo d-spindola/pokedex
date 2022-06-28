f56e0bf39199605ab49e98773176cf4ced98b21d
EhOPikachu <davispindola@proton.me>
2022-06-28 15:47:39 -0300

    [env]: add app name

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 
+const appInfo = {
+  app_name: "pokedx",
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,6 +12,9 @@ const nextConfig = {
   },
   images: {
     domains: ["raw.githubusercontent.com"],
+  },
+  env: {
+    ...appInfo,
   },
 };
 
