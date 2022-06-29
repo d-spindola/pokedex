d1687ac263db1e61c4f2986b54278cb2c23a045c
EhOPikachu <davispindola@proton.me>
2022-06-29 03:45:18 -0300

    update background

--- components/layouts/home/HomePageMainLayout.tsx
+++ components/layouts/home/HomePageMainLayout.tsx
@@ -4,7 +4,8 @@ const HomePageMainLayout = styled('main')({
   display: 'grid',
   gridTemplateColumns: '1fr',
   gridTemplateRows: '10vh 15vh auto',
-  gap: '1.0em',
+  gap: '1em',
+  background: '#ffb58c38',
   padding: '0 1em',
   height: 'min(100vh)',
 })
