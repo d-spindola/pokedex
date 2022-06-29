5e22bef97d5e3c671ad64a17c2aaa10378742f4a
EhOPikachu <davispindola@proton.me>
2022-06-28 23:52:45 -0300

    [refactor]: define rows for template

--- components/layouts/home/HomePageMainLayout.tsx
+++ components/layouts/home/HomePageMainLayout.tsx
@@ -3,9 +3,10 @@ import styled from '@emotion/styled'
 const HomePageMainLayout = styled('main')({
   display: 'grid',
   gridTemplateColumns: '1fr',
-  gap: '1.5em',
-  background: '#1d1f26',
+  gridTemplateRows: '10vh 15vh auto',
+  gap: '1.0em',
   padding: '0 1em',
+  height: 'min(100vh)',
 })
 
 export default HomePageMainLayout
