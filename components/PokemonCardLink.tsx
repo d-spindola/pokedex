2cbf2258c36d2d2e4ec97421b3503bb048271761
EhOPikachu <davispindola@proton.me>
2022-06-28 23:53:05 -0300

    [refactor]: add hover effect to cards

--- components/PokemonCardLink.tsx
+++ components/PokemonCardLink.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import { Color } from 'colorthief'
 import { FC } from 'react'
 import { Sprite } from '@lib/poke-api/types'
+import { arrayRgbToHsl, arrayToRgb } from 'utils/colors'
 
 interface Props {
   pokemonName: string
@@ -11,14 +12,21 @@ interface Props {
   sprites: Sprite
 }
 
-const Container = styled.div<{ background: string }>(({ background }) => ({
-  backgroundColor: background,
+const Container = styled.div<{ background: Color }>(({ background }) => ({
+  backgroundColor: arrayToRgb(background),
   display: 'flex',
   flexDirection: 'column',
   height: '10em',
   borderRadius: '8px',
   padding: '0 15px',
   position: 'relative',
+  border: `4px solid transparent`,
+  '&:hover': {
+    backgroundColor: arrayRgbToHsl(background, 30),
+    cursor: 'pointer',
+    border: `4px solid ${arrayRgbToHsl(background, 40)}`,
+    animation: 'all .2s ease-in-out',
+  },
   '&::before': {
     content: "''",
     backgroundImage: "url('/pokeball.png')",
@@ -48,11 +56,11 @@ const ImageContainer = styled.div({
 const PokemonCardLink: FC<Props> = ({
   pokemonName,
   types,
-  dominantColor: [r, g, b],
+  dominantColor,
   sprites,
 }) => {
   return (
-    <Container background={`rgb(${r}, ${g}, ${b})`}>
+    <Container background={dominantColor}>
       <PokemonNameContainer>
         <PokemonNameText>{pokemonName}</PokemonNameText>
       </PokemonNameContainer>
