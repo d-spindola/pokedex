6af33e97882a0ae9e27c688608c52139d67b5c1b
EhOPikachu <davispindola@proton.me>
2022-06-28 23:55:34 -0300

    [refactor]: make header accept children

--- components/layouts/shared/Header.tsx
+++ components/layouts/shared/Header.tsx
@@ -1,33 +1,34 @@
-import styled from "@emotion/styled";
-import Image from "next/image";
-import { FC } from "react";
+import styled from '@emotion/styled'
+import Image from 'next/image'
+import { FC, PropsWithChildren } from 'react'
 
 const HeaderContainer = styled.header({
-  display: "flex",
-  alignItems: "center",
-  padding: "1em 0 0",
-});
+  display: 'flex',
+  alignItems: 'center',
+  padding: '1em 0 0',
+})
 
 const LogoContainer = styled.div({
-  display: "flex",
-  alignItems: "center",
-  fontSize: "1.2em",
-  fontWeight: "bold",
-});
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: '1.2em',
+  fontWeight: 'bold',
+})
 
 interface Props {
-  appName: string;
+  appName: string
 }
 
-const Header: FC<Props> = ({ appName }) => {
+const Header: FC<PropsWithChildren<Props>> = ({ appName, children }) => {
   return (
     <HeaderContainer>
       <LogoContainer>
         <Image src="/pokeball.png" width={32} height={32} />
         <p>{appName}</p>
       </LogoContainer>
+      {children}
     </HeaderContainer>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
