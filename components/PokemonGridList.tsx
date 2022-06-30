ba8673e11af13978cdb615897265bcda8dc1314d
EhOPikachu <davispindola@proton.me>
2022-06-30 13:35:41 -0300

    [update]: move media query func to own folder

--- components/PokemonGridList.tsx
+++ components/PokemonGridList.tsx
@@ -1,15 +1,11 @@
 import styled from '@emotion/styled'
 import { FC } from 'react'
 
-const mediaQuery = (query: 'min' | 'max', value: string) => {
-  return `@media (${query}-width: ${value})`
-}
-
-const Grid = styled.div(() => ({
+const Grid = styled.div({
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fit, minmax(280px, 1fr))',
   gap: `1.5rem`,
-}))
+})
 
 interface GridProps {
   children: JSX.Element
