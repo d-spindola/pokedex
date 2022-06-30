75073ee53d50a63341b31a659b0534dfac525e44
EhOPikachu <davispindola@proton.me>
2022-06-30 13:28:02 -0300

    [fix]: remove log

--- utils/colors.ts
+++ utils/colors.ts
@@ -6,7 +6,5 @@ export const arrayToRgb = ([r, g, b]: Color): string => `rgb(${r}, ${g}, ${b})`
 export const arrayRgbToHsl = (color: Color, lightness?: number): string => {
   const [h, s, l] = RGBToHSL(color).map((v) => Math.round(v))
 
-  console.log(h, s, l)
-
   return `hsl(${h}, ${s}%, ${lightness ? lightness : l}%)`
 }
